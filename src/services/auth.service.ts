061f2eaab8171b0e45efbfb67014bec7caaf67f9
andreepdias <andre.dias@msn.com>
2020-10-15 13:54:37 -0300

    Criando página de Profile

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { JwtHelperService } from "@auth0/angular-jwt";
 import { API_CONFIG } from "../config/api.config";
 import { CredenciaisDTO } from "../models/credenciais.dto";
 import { LocalUser } from "../models/local_user";
@@ -8,9 +9,10 @@ import { StorageService } from "./storage.service";
 @Injectable()
 export class AuthService{
 
+    jwtHelper: JwtHelperService = new JwtHelperService();
+    
     constructor(public http: HttpClient, public storage: StorageService){
-
-        }
+    }
 
     authenticate(creds: CredenciaisDTO){
         return this.http.post(`${API_CONFIG.baseUrl}/login`,
@@ -24,7 +26,8 @@ export class AuthService{
     successfulLogin(authorizationValue : string){
         let tok = authorizationValue.substr(7);
         let user : LocalUser = {
-            token: tok
+            token: tok,
+            email: this.jwtHelper.decodeToken(tok).sub
         };
         this.storage.setLocalUser(user);
     }

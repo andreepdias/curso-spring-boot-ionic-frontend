0155e82eb84a218f033addb5405b910696a0066d
andreepdias <andre.dias@msn.com>
2020-10-16 18:56:38 -0300

    Criação de Página de confirmação de Pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { StorageService } from '../../services/storage.service';
 
 
 @IonicPage()
@@ -10,46 +14,46 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 })
 export class PickAddressPage {
 
-  items: EnderecoDTO [];
+  items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  pedido: PedidoDTO;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService,
+    public cartService: CartService) {}
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Quinze de Novembro",
-        numero: "300",
-        complemento: "Apto 200",
-        bairro: "Santa Mônica",
-        cep: "48293822",
-        cidade: {
-          id : "1",
-          nome: "Uberlândia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais"
+    let localUser =  this.storage.getLocalUser();
+    if(localUser && localUser.email){
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: { id: response['id'] },
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.items.map(x => { return { quantidade: x.quantidade, produto: {id: x.produto.id}} })
+          };
+        },
+        error => {
+          if(error.status == 403){
+            this.navCtrl.setRoot('HomePage');
           }
-        }
-      },
-      {
-        id: "2",
-        logradouro: "Rua Alexandre Toledo",
-        numero: "305",
-        complemento: null,
-        bairro: "Centro",
-        cep: "889333822",
-        cidade: {
-          id : "3",
-          nome: "São Paulo",
-          estado: {
-            id: "2",
-            nome: "São Paulo"
-          }
-        }
-      }
-    ]
+        });
+    }else{
+      this.navCtrl.setRoot('HomePage');
+    }
+  }
+
+  nextPage(item: EnderecoDTO){
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    this.navCtrl.push('PaymentPage', {pedido: this.pedido});
   }
 
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -9,12 +9,14 @@ import { StorageService } from "../storage.service";
 export class ClienteService{
 
     constructor(public http: HttpClient, public storage: StorageService){
-
+    }
+    
+    findById(id: string){
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
     }
 
-    findByEmail(email: string) : Observable<ClienteDTO> {
-
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+    findByEmail(email: string){
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
     getImageFromBucket(id : string) : Observable<any>{
